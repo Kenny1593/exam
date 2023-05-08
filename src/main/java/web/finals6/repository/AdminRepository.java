26634b113e709a9834e8ce8839bc5dae8ccb6817
KennyRK <114820530+Kenny1593@users.noreply.github.com>
2023-05-08 22:28:18 +0300

    delete

--- src/main/java/web/finals6/repository/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package web.finals6.repository;
-
-import web.finals6.models.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin, Integer> {
-}
--- src/main/java/web/finals6/repository/Type_pubRepository.java
+++
@@ -1,7 +0,0 @@
-package web.finals6.repository;
-
-import web.finals6.models.Type_pub;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Type_pubRepository extends JpaRepository<Type_pub,Integer> {
-}
